10f95376b716c2631bb51156a15c6b9ad15aba36
quirinal36 <turboguy36@hotmail.com>
2022-02-09 14:01:58 +0900

    test 없애기

--- src/test/java/kr/coding/lets/LetsApplicationTests.java
+++ src/test/java/kr/coding/lets/LetsApplicationTests.java
@@ -1,13 +1,6 @@
 package kr.coding.lets;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class LetsApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
